482868c1f4a8722057dc4856ea65d88bdbc06d90
Steffan Hansen <steffan.riemann@gmail.com>
2017-05-01 18:12:20 +0200

    first commit

---
+++ TestRepo/TestRepo/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepo
+{
+    class User
+    {
+        public string Name;
+        public int M;
+
+        public User(string name = "", int m = 0)
+        {
+            m = M;
+            Name = name;
+        }
+
+        public void addm(int m)
+        {
+            M = M + m;
+        }
+
+        public bool subm(int m)
+        {
+            if (M - m > 0)
+            {
+                M = M - m;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
